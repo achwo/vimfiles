87f46eb54679e6b039e4bfd277548919cab23b6a
Felix Jensen <felix.jensen@mindmatters.de>
2024-01-19 14:06:17 +0100

    Add copilot.lua

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        opts = {},
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          copilot_cmp.setup(opts)
+        end,
+      },
+    },
+  },
+}
+
