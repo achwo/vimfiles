3ee893125d014a66c50a55dd28a7dfb17fd90840
Felix Jensen <felix.jensen@mindmatters.de>
2024-01-19 14:33:52 +0100

    add conform.nvim

---
+++ lua/custom/plugins/conform.lua
@@ -0,0 +1,34 @@
+return {
+  'stevearc/conform.nvim',
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true  })
+      end,
+      mode = "",
+      desc = "Format Injected Langs"
+    },
+  },
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { { "prettierd", "prettier" } },
+      typescript = { { "prettierd", "prettier" } },
+      typescriptreact = { { "prettierd", "prettier" } },
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    -- Customize formatters
+    formatters = { },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
