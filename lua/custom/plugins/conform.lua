78bd4631e4ed16efc3abc924c1183769c2df2885
Felix Jensen <felix.jensen@mindmatters.de>
2024-09-02 08:47:36 +0200

    Upgrade deprecated conform syntax

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -18,9 +18,9 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       python = { "isort", "black" },
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
     },
     -- Set up format-on-save
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
