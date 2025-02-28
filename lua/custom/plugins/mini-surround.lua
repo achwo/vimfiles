ae918d6e79f957a055191bb838c7b62eb31eed84
Felix Jensen <felix.jensen@mindmatters.de>
2025-02-28 11:16:46 +0100

    Fix mini.surround

--- lua/custom/plugins/mini-surround.lua
+++ lua/custom/plugins/mini-surround.lua
@@ -1,32 +1,7 @@
 return {
   "echasnovski/mini.surround",
-  keys = function(_, keys)
-    -- Populate the keys based on the user's options
-    local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-    local mappings = {
-      { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
-      { opts.mappings.delete, desc = "Delete surrounding" },
-      { opts.mappings.find, desc = "Find right surrounding" },
-      { opts.mappings.find_left, desc = "Find left surrounding" },
-      { opts.mappings.highlight, desc = "Highlight surrounding" },
-      { opts.mappings.replace, desc = "Replace surrounding" },
-      { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-    }
-    mappings = vim.tbl_filter(function(m)
-      return m[1] and #m[1] > 0
-    end, mappings)
-    return vim.list_extend(mappings, keys)
-  end,
-  opts = {
-    mappings = {
-      add = "csa", -- Add surrounding in Normal and Visual modes
-      delete = "csd", -- Delete surrounding
-      find = "csf", -- Find surrounding (to the right)
-      find_left = "csF", -- Find surrounding (to the left)
-      highlight = "csh", -- Highlight surrounding
-      replace = "csr", -- Replace surrounding
-      update_n_lines = "csn", -- Update `n_lines`
-    },
-  },
+  config = function()
+    vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
+    require('mini.surround').setup()
+  end
 }
